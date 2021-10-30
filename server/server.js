1f340988c715abf9bd9fcdf25a93cbf44cb78cdd
khushalbhardwaj-0111 <khushalbhardwaj0111@gmail.com>
2021-10-30 20:13:21 +0530

    chore: mirror complete

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const httpServer = require("http").createServer();
+
+const io = require("socket.io")(httpServer, {
+	cors: {
+		origin: "http://localhost:3000",
+	},
+});
+
+const messages = [];
+
+io.on("connection", socket => {
+	messages.forEach(message => socket.emit("chat", message));
+
+	socket.on("chat", args => {
+		socket.broadcast.emit("chat", args);
+		messages.push(args);
+	});
+});
+
+httpServer.listen(3001);
